fa3874e914a8b625f962a43c95434baad8fe710d
Alain Descoux <alain@descoux.net>
2024-07-24 23:45:39 +0200

    fix header anim on scroll mobile

--- src/features/headerNav.js
+++ src/features/headerNav.js
@@ -9,22 +9,26 @@ function headerNav() {
       function () {
         const st = window.scrollY || document.documentElement.scrollTop
 
-        if (st > lastScrollTop && st > 200) {
-          // Scrolling down
-          headerNav.classList.add('is-hidden')
-          headerNav.classList.remove('is-blue')
-          /* if headerNav has a classList of 'is-home', remove it */
-        } else if (st < lastScrollTop && st > 200) {
-          headerNav.classList.remove('is-hidden')
-          headerNav.classList.add('is-blue')
-        } else if (st < lastScrollTop && st < 200) {
-          headerNav.classList.remove('is-blue')
-          headerNav.classList.remove('is-hidden')
-        } else {
-          // Scrolling up
-          headerNav.classList.remove('is-hidden')
+        if (window.innerWidth >= 991) {
+          console.log(window.innerWidth)
+          if (st > lastScrollTop && st > 200) {
+            // Scrolling down
+            headerNav.classList.add('is-hidden')
+            headerNav.classList.remove('is-blue')
+            /* if headerNav has a classList of 'is-home', remove it */
+          } else if (st < lastScrollTop && st > 200) {
+            headerNav.classList.remove('is-hidden')
+            headerNav.classList.add('is-blue')
+          } else if (st < lastScrollTop && st < 200) {
+            headerNav.classList.remove('is-blue')
+            headerNav.classList.remove('is-hidden')
+          } else {
+            // Scrolling up
+            headerNav.classList.remove('is-hidden')
+          }
+          lastScrollTop = st <= 0 ? 0 : st // For Mobile or negative scrolling
         }
-        lastScrollTop = st <= 0 ? 0 : st // For Mobile or negative scrolling
+
       },
       false
     )
